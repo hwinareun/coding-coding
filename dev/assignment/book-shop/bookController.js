037f53fed66b3825a3e7d57e162c4faf7376529a
hwinareun <hwinareun@gmail.com>
2024-06-06 19:12:22 +0900

    9w2d assignment

--- dev/assignment/book-shop/bookController.js
+++ dev/assignment/book-shop/bookController.js
@@ -1,12 +1,15 @@
 const conn = require('../mydb');
 const {StatusCodes} = require('http-status-codes');
+const jwt = require('jsonwebtoken'); 
+const ensureAuthorization = require('../auth');
 
 
 const allOfBooks = (req,res) => {
+    let allBooksRes = {};
     let {category_id, newOfBooks, limit, currentPage} = req.query;
     let offset = limit * (currentPage-1);
 
-    let sql = `SELECT * FROM books`;
+    let sql = `SELECT SQL_CALC_FOUND_ROWS *, (SELECT count(*) FROM likes WHERE books.id=liked_book_id) AS likes FROM books`;
     let values = [];
 
     if(category_id && newOfBooks){
@@ -22,41 +25,103 @@ const allOfBooks = (req,res) => {
 
     sql += `  LIMIT ? OFFSET ?`;
     values.push(parseInt(limit), offset);
-
     conn.query(sql, values,
+        (err, results) => {
+            if(err){
+                console.log(err);
+                // return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+            console.log(results);
+            if(results.length){
+                allBooksRes.books = results;
+            } else {
+                return res.status(StatusCodes.NOT_FOUND).end();
+            }
+        }
+    );
+
+    sql = `SELECT found_rows()`;
+    conn.query(sql,
         (err, results) => {
             if(err){
                 console.log(err);
                 return res.status(StatusCodes.BAD_REQUEST).end();
             }
-            if(results.length){
-                return res.status(StatusCodes.OK).json(results);
-            } else {
-                return res.status(StatusCodes.NOT_FOUND).end();
-            }
+ 
+            let pagination = {};
+            pagination.currentPage = parseInt(currentPage);
+            pagination.totalCount = results[0]["found_rows()"];
+
+            allBooksRes.pagination = pagination;
+
+            return res.status(StatusCodes.OK).json(allBooksRes);
         }
     );
 };
 
 const oneOfBooks = (req,res) => {
-    let {id} = req.params;
 
-    let sql = `SELECT * FROM books LEFT JOIN category 
-                ON books.category_id = category.id where books.id=?`;
 
-    conn.query(sql, id,
-        (err, results) => {
-            if(err){
-                console.log(err);
-                return res.status(StatusCodes.BAD_REQUEST).end();
+    let authorization = ensureAuthorization(req, res);
+
+    if(authorization instanceof jwt.TokenExpiredError){
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            message: "로그인 세션이 만료되었습니다. 다시 로그인 하세요."
+        });
+    } else if(authorization instanceof jwt.JsonWebTokenError){
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            message: "잘못된 토큰입니다."
+        });
+    } else if(authorization instanceof ReferenceError){
+        let book_id = req.params.id;
+
+        let sql = `SELECT *,
+                    (SELECT count(*) FROM likes WHERE liked_book_id=books.id) AS likes
+                        FROM books 
+                        LEFT JOIN category 
+                        ON books.category_id = category.category_id 
+                            where books.id=?`;
+        let values = [book_id];
+
+        conn.query(sql, values,
+            (err, results) => {
+                if(err){
+                    console.log(err);
+                    return res.status(StatusCodes.BAD_REQUEST).end();
+                }
+                if(results[0]){
+                    return res.status(StatusCodes.OK).json(results[0]);
+                } else {
+                    return res.status(StatusCodes.NOT_FOUND).end();
+                }
             }
-            if(results[0]){
-                return res.status(StatusCodes.OK).json(results[0]);
-            } else {
-                return res.status(StatusCodes.NOT_FOUND).end();
+        );
+    } else {
+        let book_id = req.params.id;
+
+        let sql = `SELECT *,
+                    (SELECT count(*) FROM likes WHERE liked_book_id=books.id) AS likes,
+                    (SELECT EXISTS (SELECT * FROM likes WHERE user_id=? AND liked_book_id=?)) AS liked
+                        FROM books 
+                        LEFT JOIN category 
+                        ON books.category_id = category.category_id 
+                            where books.id=?`;
+        let values = [authorization.id, book_id, book_id];
+
+        conn.query(sql, values,
+            (err, results) => {
+                if(err){
+                    console.log(err);
+                    return res.status(StatusCodes.BAD_REQUEST).end();
+                }
+                if(results[0]){
+                    return res.status(StatusCodes.OK).json(results[0]);
+                } else {
+                    return res.status(StatusCodes.NOT_FOUND).end();
+                }
             }
-        }
-    );
+        );
+    }
 };
 
 
